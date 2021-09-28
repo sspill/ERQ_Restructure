6e2d689e125d78cc512625b4332dd590b337efa4
sspill <ssp160230@utdallas.edu>
2021-09-28 14:17:53 -0500

    modified based on pull request for other repos

--- clean_ERQ_data.R
+++ clean_ERQ_data.R
@@ -1,14 +1,17 @@
 # Clean ERQ data
-# 9.14.21 SP
+# 9.28.21 SP
 
 # load required packages
 library(tidyverse)
+library(here)
 
 # load source functions
-source('/Users/ssp160230/github_directory/ERQ_Restructure/src/ERQ_functions.R')
+source(here::here('src', 'ERQ_functions.R'))
 
-# set hard-coded variables 
-erq_raw <- read_csv('/Users/ssp160230/github_directory/ERQ_Restructure/data/ERQ_data.csv')
+# set hard-coded variables
+
+# load data
+erq_raw <- read_csv(here::here('data', 'ERQ_data.csv'))
 
 # clean and reformat raw data frame
 erq_clean <- eclean(erq_raw)
@@ -16,17 +19,3 @@ erq_clean <- eclean(erq_raw)
 # calculate mean scores for cognitive restructuring 
 # and emotional suppression
 erq_mean <- emean(erq_clean)
-
-#write csv for clean df
-write.csv(
-  erq_clean, 
-  "/Users/ssp160230/github_directory/ERQ_Restructure/output/ERQ_clean.csv", 
-  row.names = FALSE
-)
-
-# write csv for mean scores df
-write.csv(
-  erq_mean, 
-  "/Users/ssp160230/github_directory/ERQ_Restructure/output/ERQ_mean.csv", 
-  row.names = FALSE
-)
